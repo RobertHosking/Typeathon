3e6c80cb17d0074d278f73d0fc2f931207ae11b9
Rick Sanchez <buzz@off.lol>
2019-05-21 11:47:47 -0400

    basic express server

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const MonogClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser';
+const bodyParser = require('body-parser');
         
 const app = express();
+
+const port = 80;
+app.listen(port, () =>{
+    console.log("we are live on " + port)
+});
